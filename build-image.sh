434e17f74abe588af3f804cb4d3227bb395f088b
devangparikh <dparikh1@gmail.com>
2021-12-09 05:37:04 -0800

    use IMAGE_TAG

--- build-image.sh
+++ build-image.sh
@@ -5,6 +5,10 @@ if [[ -z "${JAZZ_TEAM_SERVER}" ]]; then
   exit 1
 fi
 
+if [[ -z "${IMAGE_TAG}" ]]; then
+  IMAGE_TAG=latest
+fi
+
 if [ ! -d "${JAZZ_TEAM_SERVER}" ] 
 then
   echo "environment variable JAZZ_TEAM_SERVER must point to JazzTeamServer directory"
@@ -25,7 +29,7 @@ cp -R ${JAZZ_TEAM_SERVER} ./copy/JazzTeamServer
 sed -i 's/<keyStore id="defaultKeyStore".*/<keyStore id="defaultKeyStore" location="${keystore_pkcs12}" type="PKCS12" password="${keystore_pw}"\/>/g' ./copy/JazzTeamServer/server/liberty/clmServerTemplate/server.xml
 mv ./copy/JazzTeamServer/server/conf ./copy/JazzTeamServer/server/confTemplate
 
-docker build -t elm-ocp:latest -f ./docker/Dockerfile .
+docker build -t elm-ocp:${IMAGE_TAG} -f ./docker/Dockerfile .
 docker login -u ${REGISTRY_USERNAME} -p ${REGISTRY_PASSWORD} ${REGISTRY_URL}
-docker tag elm-ocp:latest ${REGISTRY_URL}/elm-ocp:latest
-docker push ${REGISTRY_URL}/elm-ocp:latest
+docker tag elm-ocp:${IMAGE_TAG} ${REGISTRY_URL}/elm-ocp:${IMAGE_TAG}
+docker push ${REGISTRY_URL}/elm-ocp:${IMAGE_TAG}
